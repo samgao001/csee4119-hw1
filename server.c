58dac12ba898586cf3b629a7f98b50fad78832a5
X <sam_001.gao@yahoo.com>
2014-09-28 13:52:32 -0400

    load user info file completed

--- server.c
+++ server.c
@@ -11,25 +11,77 @@
 #include <getopt.h>    		// for getopt_long
 #include <pthread.h> //for threading , link with lpthread
 
-#define user_file			"user_pass.txt"
+#define USER_FILE			"user_pass.txt"
+#define MAX_USER_NUMBER		256
+#define PASS_SIZE			32
 
-char* user[1024];
-char* pass[1024];
+char user[MAX_USER_NUMBER][PASS_SIZE];
+char pass[MAX_USER_NUMBER][PASS_SIZE];
+int user_count;
 
 void load_user_info(void);
+void error(char* str);
 
 int main(int argc, char* argv[])
 {
-	int socket, new_socket, port_number, client_len;
+	int socket_id, new_socket_id, port_number, client_len;
 	char buffer[256];
-	struct sockaddr_in, server_addr, client_addr;	
+	struct sockaddr_in server_addr, client_addr;
+	
+	if(argc <2)
+	{
+		error("No port provided");
+		exit(EXIT_FAILURE);
+	}	
+
+	printf("%s\n", argv[1]);
 
 	load_user_info();
 
-	exit(0);
+	exit(EXIT_SUCCESS);
+}
+
+void error(char* str)
+{
+  fprintf(stderr, "ERROR: %s\n", str);
 }
 
 void load_user_info(void)
 {
-	
+	FILE* inFile;
+	int item_read = 0;
+	user_count = 0;
+
+	inFile = fopen(USER_FILE, "r");
+
+	if(inFile == NULL)
+	{
+	   error("Failed to open input file, please check if such file exist.");
+	   exit(EXIT_FAILURE);
+	}
+
+	//keep reading until end of the file
+	while(!feof(inFile)) 
+	{
+		item_read = fscanf(inFile, "%s %s", user[user_count], pass[user_count]);
+		user_count++;
+		
+		// check if it reads an empty line
+		if(item_read == -1)
+		{
+			// decrement the line count whenever an empty line is reached
+			// or end of the file is reached. if eof terminates the loop.
+			user_count--;
+			if(feof(inFile)) break;
+		}
+		else if(item_read != 2)
+		{
+			error("Failed to read user info.");
+			exit(EXIT_FAILURE);
+			break;
+		}
+	}
+
+	//close file and free up memory
+	fclose(inFile);
 }
